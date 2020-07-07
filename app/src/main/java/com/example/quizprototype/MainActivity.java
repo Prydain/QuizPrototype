6e18b147ba77319cdd6f8e875bbc1eb029023c1a
joewatt40 <wat19030@byui.edu>
2020-07-06 23:49:24 -0700

    update results ui and buttons

--- app/src/main/java/com/example/quizprototype/MainActivity.java
+++ app/src/main/java/com/example/quizprototype/MainActivity.java
@@ -1,12 +1,10 @@
 package com.example.quizprototype;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Parcelable;
 import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     private String category;
